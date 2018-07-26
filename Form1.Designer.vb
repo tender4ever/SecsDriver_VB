9253f8e280eed1a096ed51157b4247069da8d0fc
tender4ever <tender4ever.young@gmail.com>
2018-07-26 16:41:16 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form 覆寫 Dispose 以清除元件清單。
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'為 Windows Form 設計工具的必要項
+	Private components As System.ComponentModel.IContainer
+
+	'注意: 以下為 Windows Form 設計工具所需的程序
+	'可以使用 Windows Form 設計工具進行修改。
+	'請勿使用程式碼編輯器進行修改。
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.btnDisconnect = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.lblThreadSleep = New System.Windows.Forms.Label()
+        Me.lblEntity = New System.Windows.Forms.Label()
+        Me.tbxThreadSleep = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblPrimarySent = New System.Windows.Forms.Label()
+        Me.lblSecondaryReceived = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblPrimaryReceived = New System.Windows.Forms.Label()
+        Me.lblSecondarySent = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.tbxTotalSend = New System.Windows.Forms.TextBox()
+        Me.btnStopSend = New System.Windows.Forms.Button()
+        Me.btnClean = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(12, 29)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnConnect.TabIndex = 6
+        Me.btnConnect.Text = "連線"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'btnDisconnect
+        '
+        Me.btnDisconnect.Location = New System.Drawing.Point(12, 58)
+        Me.btnDisconnect.Name = "btnDisconnect"
+        Me.btnDisconnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnDisconnect.TabIndex = 7
+        Me.btnDisconnect.Text = "斷線"
+        Me.btnDisconnect.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 159)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(620, 484)
+        Me.RichTextBox1.TabIndex = 8
+        Me.RichTextBox1.Text = ""
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(108, 29)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(100, 23)
+        Me.btnSend.TabIndex = 9
+        Me.btnSend.Text = "Send Message"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'lblThreadSleep
+        '
+        Me.lblThreadSleep.AutoSize = True
+        Me.lblThreadSleep.Location = New System.Drawing.Point(106, 97)
+        Me.lblThreadSleep.Name = "lblThreadSleep"
+        Me.lblThreadSleep.Size = New System.Drawing.Size(66, 12)
+        Me.lblThreadSleep.TabIndex = 10
+        Me.lblThreadSleep.Text = "Thread Sleep"
+        '
+        'lblEntity
+        '
+        Me.lblEntity.AutoSize = True
+        Me.lblEntity.Location = New System.Drawing.Point(12, 9)
+        Me.lblEntity.Name = "lblEntity"
+        Me.lblEntity.Size = New System.Drawing.Size(37, 12)
+        Me.lblEntity.TabIndex = 11
+        Me.lblEntity.Text = "Label1"
+        '
+        'tbxThreadSleep
+        '
+        Me.tbxThreadSleep.Location = New System.Drawing.Point(178, 92)
+        Me.tbxThreadSleep.Name = "tbxThreadSleep"
+        Me.tbxThreadSleep.Size = New System.Drawing.Size(50, 22)
+        Me.tbxThreadSleep.TabIndex = 12
+        Me.tbxThreadSleep.Text = "500"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(248, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 12)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "PrimarySent :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(248, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(103, 12)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "SecondaryReceived :"
+        '
+        'lblPrimarySent
+        '
+        Me.lblPrimarySent.AutoSize = True
+        Me.lblPrimarySent.ForeColor = System.Drawing.Color.Red
+        Me.lblPrimarySent.Location = New System.Drawing.Point(397, 20)
+        Me.lblPrimarySent.Name = "lblPrimarySent"
+        Me.lblPrimarySent.Size = New System.Drawing.Size(11, 12)
+        Me.lblPrimarySent.TabIndex = 15
+        Me.lblPrimarySent.Text = "0"
+        '
+        'lblSecondaryReceived
+        '
+        Me.lblSecondaryReceived.AutoSize = True
+        Me.lblSecondaryReceived.ForeColor = System.Drawing.Color.Red
+        Me.lblSecondaryReceived.Location = New System.Drawing.Point(397, 55)
+        Me.lblSecondaryReceived.Name = "lblSecondaryReceived"
+        Me.lblSecondaryReceived.Size = New System.Drawing.Size(11, 12)
+        Me.lblSecondaryReceived.TabIndex = 16
+        Me.lblSecondaryReceived.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(248, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 12)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "PrimaryReceived :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(248, 126)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 12)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "SecondarySent : "
+        '
+        'lblPrimaryReceived
+        '
+        Me.lblPrimaryReceived.AutoSize = True
+        Me.lblPrimaryReceived.ForeColor = System.Drawing.Color.Red
+        Me.lblPrimaryReceived.Location = New System.Drawing.Point(397, 90)
+        Me.lblPrimaryReceived.Name = "lblPrimaryReceived"
+        Me.lblPrimaryReceived.Size = New System.Drawing.Size(11, 12)
+        Me.lblPrimaryReceived.TabIndex = 19
+        Me.lblPrimaryReceived.Text = "0"
+        '
+        'lblSecondarySent
+        '
+        Me.lblSecondarySent.AutoSize = True
+        Me.lblSecondarySent.ForeColor = System.Drawing.Color.Red
+        Me.lblSecondarySent.Location = New System.Drawing.Point(397, 125)
+        Me.lblSecondarySent.Name = "lblSecondarySent"
+        Me.lblSecondarySent.Size = New System.Drawing.Size(11, 12)
+        Me.lblSecondarySent.TabIndex = 20
+        Me.lblSecondarySent.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(106, 132)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(55, 12)
+        Me.Label5.TabIndex = 21
+        Me.Label5.Text = "Total Send"
+        '
+        'tbxTotalSend
+        '
+        Me.tbxTotalSend.Location = New System.Drawing.Point(178, 126)
+        Me.tbxTotalSend.Name = "tbxTotalSend"
+        Me.tbxTotalSend.Size = New System.Drawing.Size(50, 22)
+        Me.tbxTotalSend.TabIndex = 22
+        Me.tbxTotalSend.Text = "0"
+        '
+        'btnStopSend
+        '
+        Me.btnStopSend.Location = New System.Drawing.Point(108, 58)
+        Me.btnStopSend.Name = "btnStopSend"
+        Me.btnStopSend.Size = New System.Drawing.Size(100, 23)
+        Me.btnStopSend.TabIndex = 23
+        Me.btnStopSend.Text = "Stop Send"
+        Me.btnStopSend.UseVisualStyleBackColor = True
+        '
+        'btnClean
+        '
+        Me.btnClean.Location = New System.Drawing.Point(557, 132)
+        Me.btnClean.Name = "btnClean"
+        Me.btnClean.Size = New System.Drawing.Size(75, 23)
+        Me.btnClean.TabIndex = 24
+        Me.btnClean.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(644, 655)
+        Me.Controls.Add(Me.btnClean)
+        Me.Controls.Add(Me.btnStopSend)
+        Me.Controls.Add(Me.tbxTotalSend)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblSecondarySent)
+        Me.Controls.Add(Me.lblPrimaryReceived)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblSecondaryReceived)
+        Me.Controls.Add(Me.lblPrimarySent)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tbxThreadSleep)
+        Me.Controls.Add(Me.lblEntity)
+        Me.Controls.Add(Me.lblThreadSleep)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.btnDisconnect)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnConnect As Button
+	Friend WithEvents btnDisconnect As Button
+	Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents btnSend As Button
+    Friend WithEvents lblThreadSleep As Label
+    Friend WithEvents lblEntity As Label
+    Friend WithEvents tbxThreadSleep As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblPrimarySent As Label
+    Friend WithEvents lblSecondaryReceived As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblPrimaryReceived As Label
+    Friend WithEvents lblSecondarySent As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents tbxTotalSend As TextBox
+    Friend WithEvents btnStopSend As Button
+    Friend WithEvents btnClean As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,339 @@
+﻿Imports System
+Imports System.Text
+Imports SecsDriver_VB.SecsDriver
+Imports System.Threading
+
+
+Public Class Form1
+
+    Dim aWrapper As SecsWrapper
+
+    Private startSend As Thread
+
+    Private cleanTextBox As Thread
+
+    Private StopSend As Boolean
+    Private StopClean As Boolean
+
+    Private aPrimarySent As Integer = 0
+    Private aPrimaryReceived As Integer = 0
+    Private aSecondarySent As Integer = 0
+    Private aSecondaryReceived As Integer = 0
+
+
+    Public Delegate Sub UpdateGUI(ByVal sysMessage As String, ByRef control As Control)
+
+    Public Delegate Sub CleanGUI(ByRef control As Control)
+
+
+    Public Sub New()
+
+        ' 設計工具需要此呼叫。
+        InitializeComponent()
+
+        ' 在 InitializeComponent() 呼叫之後加入所有初始設定。
+
+        btnClean.Text = "Start Clean"
+
+    End Sub
+
+
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+
+        aWrapper = New SecsWrapper()
+
+        Dim sDelegateSecsConnect As New delegateSecsConnectStateChange(AddressOf SecsConnect)
+        Dim sDelegatePrimaryReceive As New delegatePrimaryReceived(AddressOf PrimaryReceive)
+        Dim sDelegatePrimarySent As New delegatePrimarySent(AddressOf PrimarySent)
+        Dim sDelegateSecondaryReceive As New delegateSecondaryReceived(AddressOf SecondaryReceive)
+        Dim sDelegateSecondarySent As New delegateSecondarySent(AddressOf SecondarySent)
+        Dim sDelegateMessageError As New delegateMessageError(AddressOf ErrorMessage)
+        Dim sDelegateMessageInfo As New delegateMessageInfo(AddressOf MessageInfo)
+        Dim sDelegateSecsError As New delegateError(AddressOf SecsError)
+        Dim sDelegateTimeout As New delegateTimeout(AddressOf HandleTimeout)
+
+        AddHandler aWrapper.OnSecsConnectStateChange, sDelegateSecsConnect
+        AddHandler aWrapper.OnPrimaryReceived, sDelegatePrimaryReceive
+        AddHandler aWrapper.OnPrimarySent, sDelegatePrimarySent
+        AddHandler aWrapper.OnSecondaryReceived, sDelegateSecondaryReceive
+        AddHandler aWrapper.OnSecondarySent, sDelegateSecondarySent
+        AddHandler aWrapper.OnMessageError, sDelegateMessageError
+        AddHandler aWrapper.OnMessageInfo, sDelegateMessageInfo
+        AddHandler aWrapper.OnError, sDelegateSecsError
+        AddHandler aWrapper.OnTimeout, sDelegateTimeout
+
+        aWrapper.Open()
+
+        If aWrapper.siniFile IsNot Nothing Then
+            lblEntity.Text = aWrapper.siniFile.Entity.ToString
+        End If
+
+    End Sub
+
+
+    Private Sub btnDisconnect_Click(sender As Object, e As EventArgs) Handles btnDisconnect.Click
+
+        StopSend = True
+        aWrapper.Close()
+
+    End Sub
+
+
+    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+
+        startSend = New Thread(AddressOf SendMessage)
+        startSend.IsBackground = True
+        startSend.Start()
+
+    End Sub
+
+
+    Private Sub btnStopSend_Click(sender As Object, e As EventArgs) Handles btnStopSend.Click
+
+        StopSend = True
+
+    End Sub
+
+
+    Private Sub btnClean_Click(sender As Object, e As EventArgs) Handles btnClean.Click
+
+        If btnClean.Text = "Start Clean" Then
+
+            cleanTextBox = New Thread(AddressOf CleanTbx)
+            cleanTextBox.IsBackground = True
+            cleanTextBox.Start()
+
+            btnClean.Text = "Stop Clean"
+        Else
+            StopClean = True
+
+            btnClean.Text = "Start Clean"
+
+        End If
+
+
+    End Sub
+
+
+    Private Sub SendMessage()
+
+        StopSend = False
+
+        Dim sendcount As Integer
+
+        If tbxTotalSend.Text <> "" Then
+
+            sendcount = Convert.ToInt64(tbxTotalSend.Text)
+
+        End If
+
+        Do
+            If tbxTotalSend.Text = "" Then
+
+                Dim temp As SecsMessage = aWrapper.GetMessageByName(random())
+                aWrapper.SendPrimary(temp)
+
+                Application.DoEvents()
+                Dim tempTime As Integer = Convert.ToInt16(tbxThreadSleep.Text)
+                Thread.Sleep(tempTime)
+
+            ElseIf sendcount = 0 Then
+
+                StopSend = True
+
+            Else
+
+                Dim temp As SecsMessage = aWrapper.GetMessageByName(random())
+                aWrapper.SendPrimary(temp)
+
+                Application.DoEvents()
+                Dim tempTime As Integer = Convert.ToInt16(tbxThreadSleep.Text)
+                Thread.Sleep(tempTime)
+
+                sendcount = sendcount - 1
+
+            End If
+
+        Loop Until StopSend = True
+
+    End Sub
+
+
+    Private Sub CleanTbx()
+
+        StopClean = False
+
+        Do
+            Dim temp As CleanGUI = New CleanGUI(AddressOf GUIClean)
+            Invoke(temp, RichTextBox1)
+
+            Application.DoEvents()
+            Thread.Sleep(10000)
+
+        Loop Until StopClean = True
+
+    End Sub
+
+
+    Private Function random() As String
+
+        Dim temp As List(Of String) = New List(Of String)
+
+        temp.Add("S1F1_AreYouThere")
+        temp.Add("S1F3_SelectedEquipmentStatusRequest")
+        temp.Add("S1F11_StatusVariableNamelistRequest")
+        temp.Add("S1F13_CommMCSRequest")
+        temp.Add("S1F13_CommStkcRequest")
+        temp.Add("S1F15_MCSOffnlineRequest")
+        temp.Add("S1F17_OnlineRequestFromMCS")
+        temp.Add("S2F13_EquipmentConstantRequest")
+        temp.Add("S2F15_NewEquipmentConstantRequest")
+        temp.Add("S2F17_DateTimeRequest")
+        temp.Add("S2F21_RemoteCommandRequest")
+        temp.Add("S2F25_LoopbackDiagnosticRequest")
+        temp.Add("S2F29_EquipmentConstantNamelistRequest")
+        temp.Add("S2F31_DateTimeSetRequest")
+        temp.Add("S2F33_DefineReport")
+        temp.Add("S2F35_LinkEventReport")
+        temp.Add("S2F37_EnableDisableEventReport")
+        temp.Add("S2F41_CarrierTypeSetCommand")
+        temp.Add("S2F41_RemoteAbortCommand")
+        temp.Add("S2F41_RemoteBlockCommand")
+        temp.Add("S2F41_RemoteCancelCommand")
+        temp.Add("S2F41_RemoteIDReaderModeChangeCommand")
+        temp.Add("S2F41_RemoteInstallCommand")
+        temp.Add("S2F41_RemoteLocateCommand")
+        temp.Add("S2F49_RemoteTransferCommand")
+        temp.Add("S5F1_AlarmReport")
+        temp.Add("S5F3_EnableAlarmSend")
+        temp.Add("S5F5_ListAlarmRequest")
+        temp.Add("S5F7_ListEnabledAlarmRequest")
+        temp.Add("S6F11_ActiveCarrierReq")
+        temp.Add("S6F11_Alarm")
+        temp.Add("S6F11_AlarmClear")
+        temp.Add("S6F11_AlarmSet")
+
+        Dim a As Random = New Random
+        Dim b As Integer = a.Next(0, temp.Count)
+        Return temp(b)
+
+    End Function
+
+
+    Public Sub SecsConnect(ByRef aMessage As String)
+
+        Dim temp As UpdateGUI = New UpdateGUI(AddressOf GUIUpdate)
+        Invoke(temp, "Secs Connect State : " & aMessage, RichTextBox1)
+
+    End Sub
+
+
+    Public Sub PrimaryReceive(ByRef aTransaction As SecsTransaction)
+
+        Dim temp As UpdateGUI = New UpdateGUI(AddressOf GUIUpdate)
+        Invoke(temp, "Primary Receive : " & aTransaction.Primary.MessageName, RichTextBox1)
+
+        aPrimaryReceived = aPrimaryReceived + 1
+        Invoke(temp, "PrimaryReceived", lblPrimaryReceived)
+
+    End Sub
+
+
+    Public Sub PrimarySent(ByRef aTransaction As SecsTransaction)
+
+        Dim temp As UpdateGUI = New UpdateGUI(AddressOf GUIUpdate)
+        Invoke(temp, "Primary Sent : " & aTransaction.Primary.MessageName, RichTextBox1)
+
+        aPrimarySent = aPrimarySent + 1
+        Invoke(temp, "PrimarySent", lblPrimarySent)
+
+    End Sub
+
+
+    Public Sub SecondaryReceive(ByRef aTransaction As SecsTransaction)
+
+        Dim temp As UpdateGUI = New UpdateGUI(AddressOf GUIUpdate)
+        Invoke(temp, "Secondary Receive : " & aTransaction.Secondary.MessageName, RichTextBox1)
+        aSecondaryReceived = aSecondaryReceived + 1
+        Invoke(temp, "SecondaryReceived", lblSecondaryReceived)
+
+    End Sub
+
+
+    Public Sub SecondarySent(ByRef aTransaction As SecsTransaction)
+
+        Dim temp As UpdateGUI = New UpdateGUI(AddressOf GUIUpdate)
+        Invoke(temp, "Secondary Sent : " & aTransaction.Secondary.MessageName, RichTextBox1)
+        aSecondarySent = aSecondarySent + 1
+        Invoke(temp, "SecondarySent", lblSecondarySent)
+
+    End Sub
+
+
+    Public Sub ErrorMessage(ByRef message As SecsMessage, ByVal errorMessage As String)
+
+        Dim temp As UpdateGUI = New UpdateGUI(AddressOf GUIUpdate)
+
+        If message.MessageName Is Nothing Then
+            Invoke(temp, "Control Message " & errorMessage, RichTextBox1)
+        Else
+            Invoke(temp, message.MessageName & " : " & errorMessage, RichTextBox1)
+        End If
+
+    End Sub
+
+
+    Public Sub MessageInfo(ByRef message As SecsMessage, ByVal aMessage As String)
+
+        Dim temp As UpdateGUI = New UpdateGUI(AddressOf GUIUpdate)
+        Invoke(temp, message.MessageName & " : " & aMessage, RichTextBox1)
+
+    End Sub
+
+
+    Public Sub SecsError(ByRef aMessage As String)
+
+        Dim temp As UpdateGUI = New UpdateGUI(AddressOf GUIUpdate)
+        Invoke(temp, aMessage, RichTextBox1)
+
+    End Sub
+
+
+    Public Sub HandleTimeout(ByRef aMessage As String)
+
+        Dim temp As UpdateGUI = New UpdateGUI(AddressOf GUIUpdate)
+        Invoke(temp, aMessage, RichTextBox1)
+
+    End Sub
+
+
+    Public Sub GUIUpdate(ByVal sysMessage As String, ByRef control As Control)
+
+        If sysMessage = "PrimarySent" Then
+            lblPrimarySent.Text = aPrimarySent.ToString
+
+        ElseIf sysMessage = "PrimaryReceived" Then
+            lblPrimaryReceived.Text = aPrimaryReceived.ToString
+
+        ElseIf sysMessage = "SecondarySent" Then
+            lblSecondarySent.Text = aSecondarySent.ToString
+
+        ElseIf sysMessage = "SecondaryReceived" Then
+            lblSecondaryReceived.Text = aSecondaryReceived.ToString
+
+        Else
+            RichTextBox1.Text = RichTextBox1.Text & Now.ToString("yyyy/MM/dd hh:mm:ss.fff") & " : " & sysMessage & vbLf
+        End If
+
+    End Sub
+
+
+    Public Sub GUIClean(ByRef control As Control)
+
+        RichTextBox1.Text = ""
+
+    End Sub
+
+
+
+End Class
